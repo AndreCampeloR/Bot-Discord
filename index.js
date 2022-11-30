14be441425c4609542787c6c5e1bed2755b24576
AndreCampeloR <decocampeloreis@gmail.com>
2022-11-30 15:35:32 -0300

    att cod

--- index.js
+++ index.js
@@ -45,22 +45,6 @@ for (const file of commandFiles) {
 
 // quiz
 
-const quiz = require('./quiz.json');
-const item = quiz[Math.floor(Math.random() * quiz.length)];
-const filter = response => {
-	return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase());
-};
-
-interaction.reply({ content: item.question, fetchReply: true })
-	.then(() => {
-		interaction.channel.awaitMessages({ filter, max: 1, time: 30000, errors: ['time'] })
-			.then(collected => {
-				interaction.followUp(`${collected.first().author} got the correct answer!`);
-			})
-			.catch(collected => {
-				interaction.followUp('Looks like nobody got the answer this time.');
-			});
-	});
 
 
 // Log in to Discord with your client's token
