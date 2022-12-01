a89c28be99829bf18ba18f3e9fdcb3e655192b52
AndreCampeloR <decocampeloreis@gmail.com>
2022-12-01 13:38:51 -0300

    correção de bugs

--- index.js
+++ index.js
@@ -1,57 +1,78 @@
 
 const prefixo = '$'
 
-// Require the necessary discord.js classes
+require('dotenv').config();
 
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits, Message, Events } = require('discord.js');
-const { token } = require('./config.json');
+const {REST} = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const { Client, GatewayIntentBits, Collection } = require('discord.js')
+const { Player } = require('discord-player')
 
-// Create a new client instance
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const fs = require('fs');
+const path = require('path');
+
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+
+// List of all commands
+const commands = [];
 client.commands = new Collection();
 
-// eventos
-
-const eventsPath = path.join(__dirname, 'events');
-const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-for (const file of eventFiles) {
-	const filePath = path.join(eventsPath, file);
-	const event = require(filePath);
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args));
-	} else {
-		client.on(event.name, (...args) => event.execute(...args));
-	}
-}
-
-// comandos
-
-const commandsPath = path.join(__dirname, 'commands');
+const commandsPath = path.join(__dirname, "commands"); // E:\yt\discord bot\js\intro\commands
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+for(const file of commandFiles)
+{
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
 
-for (const file of commandFiles) {
-	const filePath = path.join(commandsPath, file);
-	const command = require(filePath);
-	// Set a new item in the Collection with the key as the command name and the value as the exported module
-	if ('data' in command && 'execute' in command) {
-		client.commands.set(command.data.name, command);
-	} else {
-		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-	}
+    client.commands.set(command.data.name, command);
+    commands.push(command.data.toJSON());
 }
 
-// comandos de chat
+// Add the player on the client
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+client.on("ready", () => {
+    // Get all ids of the servers
+    const guild_ids = client.guilds.cache.map(guild => guild.id);
+
+
+    const rest = new REST({version: '9'}).setToken(process.env.TOKEN);
+    for (const guildId of guild_ids)
+    {
+        rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId), 
+            {body: commands})
+        .then(() => console.log('Successfully updated commands for guild ' + guildId))
+        .catch(console.error);
+    }
+});
+
+client.on("interactionCreate", async interaction => {
+    if(!interaction.isCommand()) return;
+
+    const command = client.commands.get(interaction.commandName);
+    if(!command) return;
+
+    try
+    {
+        await command.execute({client, interaction});
+    }
+    catch(error)
+    {
+        console.error(error);
+        await interaction.reply({content: "There was an error executing this command"});
+    }
+});
+
+client.login(process.env.TOKEN);
 
 
 
-// quiz
-
-
-// Log in to Discord with your client's token
-client.login(token);
 
 
 
@@ -66,30 +87,66 @@ client.login(token);
 
 
 
-// const Discord = require('discord.js');
-// const client = new Discord.Client();
 
-// client.on("ready", () => {
-//     console.log('estou online!!');
-// });
 
-// client.on("message", async (msg) => {
 
-//     // filtros
-//      if(!msg.guild) return;
-     
-//      if(!msg.content.startsWith(prefixo)) return;
 
-//     // comandos
-//     if(msg.content == prefixo + 'join') {  //$join
-//        await msg.member.voice.channel.join();
-//     }
-//     if(msg.content == 'oi') {
-//        msg.reply('olá!');
-//     }
-//     if(msg.content == 'olá') {
-//        msg.channel.send('olá ' + msg.author.username);
-//     }
-// });
+// // ----- Require the necessary discord.js classes -----
 
-// client.login(TOKEN);
+// const fs = require('node:fs');
+// const path = require('node:path');
+// const { Client, Collection, GatewayIntentBits, Message, Events } = require('discord.js');
+// const { token } = require('./config.json');
+
+// // ----- Create a new client instance ----- 
+// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// client.commands = new Collection();
+
+// // eventos
+
+// const eventsPath = path.join(__dirname, 'events');
+// const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+// for (const file of eventFiles) {
+// 	const filePath = path.join(eventsPath, file);
+// 	const event = require(filePath);
+// 	if (event.once) {
+// 		client.once(event.name, (...args) => event.execute(...args));
+// 	} else {
+// 		client.on(event.name, (...args) => event.execute(...args));
+// 	}
+// }
+
+// // ----- comandos ------
+
+// const commandsPath = path.join(__dirname, 'commands');
+// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+// for (const file of commandFiles) {
+// 	const filePath = path.join(commandsPath, file);
+// 	const command = require(filePath);
+// 	// ------ Set a new item in the Collection with the key as the command name and the value as the exported module -----
+// 	if ('data' in command && 'execute' in command) {
+// 		client.commands.set(command.data.name, command);
+// 	} else {
+// 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+// 	}
+// }
+
+
+// // ------ Log in to Discord with your client's token -----
+// client.login(token);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
