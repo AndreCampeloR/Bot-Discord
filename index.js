36517ad009b3978dd29f24e0c503d7a421179942
AndreCampeloR <decocampeloreis@gmail.com>
2022-12-02 11:55:40 -0300

    add quiz

--- index.js
+++ index.js
@@ -5,14 +5,19 @@ require('dotenv').config();
 
 const {REST} = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const { Client, GatewayIntentBits, Collection } = require('discord.js')
+const { Client, GatewayIntentBits, Collection, User, InteractionType } = require('discord.js')
 const { Player } = require('discord-player')
 
 const fs = require('fs');
 const path = require('path');
 
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds, 
+                                      GatewayIntentBits.GuildVoiceStates,
+                                      GatewayIntentBits.DirectMessages,
+                                      GatewayIntentBits.GuildMessages,
+                                      GatewayIntentBits.MessageContent,
+                                      ] });
 
 // List of all commands
 const commands = [];
@@ -47,7 +52,7 @@ client.on("ready", () => {
     {
         rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, guildId), 
             {body: commands})
-        .then(() => console.log('Successfully updated commands for guild ' + guildId))
+        .then(() => console.log('Successfully! O pai tá on'))
         .catch(console.error);
     }
 });
@@ -68,6 +73,7 @@ client.on("interactionCreate", async interaction => {
         await interaction.reply({content: "There was an error executing this command"});
     }
 });
+
 
 client.login(process.env.TOKEN);
 
@@ -90,52 +96,6 @@ client.login(process.env.TOKEN);
 
 
 
-
-// // ----- Require the necessary discord.js classes -----
-
-// const fs = require('node:fs');
-// const path = require('node:path');
-// const { Client, Collection, GatewayIntentBits, Message, Events } = require('discord.js');
-// const { token } = require('./config.json');
-
-// // ----- Create a new client instance ----- 
-// const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-// client.commands = new Collection();
-
-// // eventos
-
-// const eventsPath = path.join(__dirname, 'events');
-// const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
-
-// for (const file of eventFiles) {
-// 	const filePath = path.join(eventsPath, file);
-// 	const event = require(filePath);
-// 	if (event.once) {
-// 		client.once(event.name, (...args) => event.execute(...args));
-// 	} else {
-// 		client.on(event.name, (...args) => event.execute(...args));
-// 	}
-// }
-
-// // ----- comandos ------
-
-// const commandsPath = path.join(__dirname, 'commands');
-// const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-
-// for (const file of commandFiles) {
-// 	const filePath = path.join(commandsPath, file);
-// 	const command = require(filePath);
-// 	// ------ Set a new item in the Collection with the key as the command name and the value as the exported module -----
-// 	if ('data' in command && 'execute' in command) {
-// 		client.commands.set(command.data.name, command);
-// 	} else {
-// 		console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-// 	}
-// }
-
-
-// // ------ Log in to Discord with your client's token -----
-// client.login(token);
 
 
 
